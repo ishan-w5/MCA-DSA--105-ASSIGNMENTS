58554495e146740b11f120e9b032ea8374cbf263
Ishan <wadhwaishan2005@gmail.com>
2026-09-14 16:24:10 +0530

    Add files via upload

---
+++ Assignment-3/Program4.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int precedence(char op) {
+    if (op == '^')
+        return 3;
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '+' || op == '-')
+        return 1;
+    return 0;
+}
+
+string infixToPostfix(string infix) {
+    stack<char> s;
+    string postfix = "";
+
+    for (int i = 0; i < (int)infix.length(); i++) {
+        char ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix += ch;
+        }
+        else if (ch == '(') {
+            s.push(ch);
+        }
+        else if (ch == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            if (!s.empty())
+                s.pop(); 
+        }
+        else {
+            while (!s.empty() &&
+                   s.top() != '(' &&
+                   precedence(s.top()) >= precedence(ch)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(ch);
+        }
+    }
+
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    string infix;
+
+    cout << "Enter an infix expression: ";
+    cin >> infix;
+
+    string postfix = infixToPostfix(infix);
+
+    cout << "Postfix expression: " << postfix << endl;
+
+    return 0;
+}
+
